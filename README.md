4cd32599c937be33a2af86b6e4bc6dce4ffeb239
Matt Wilson <matthew@synesis.com.au>
2024-04-02 10:25:21 +1100

    ~ minor tidying, and some project boilerplate

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# misc-dev-scripts
+# misc-dev-scripts <!-- omit in toc -->
+
 Miscellaneous Development Scripts
+
+
+## Table of Contents <!-- omit in toc -->
+
+- [Bash Scripts](#bash-scripts)
+  - [run\_all\_unit\_tests.sh](#run_all_unit_testssh)
+- [Related projects](#related-projects)
+
 
 ## Bash Scripts
 
@@ -10,7 +19,7 @@ This script provided for projects in the following languages/technologies:
 * [Python](./bash/run_all_unit_tests.sh/python/run_all_unit_tests.sh)
 * [Ruby](./bash/run_all_unit_tests.sh/ruby/run_all_unit_tests.sh)
 
-**NOTE**: use the flag `--help` in the **Ruby** variant to see the various execution options, which include the flag `--rbenv-versions` to run tests across all installed versions of the language.
+> **NOTE**: use the flag `--help` in the **Ruby** variant to see the various execution options, which include the flag `--rbenv-versions` to run tests across all installed versions of the language.
 
 
 ## Related projects
@@ -19,9 +28,13 @@ Resources from this project are used in the following projects:
 
 * the [**CLASP.Python**](https://github.com/synesissoftware/CLASP.Ruby) library;
 * the [**CLASP.Ruby**](https://github.com/synesissoftware/CLASP.Ruby) library;
+* the [**cmpfs.Ruby**](https://github.com/synesissoftware/cmpfs.Ruby) library;
 * the [**libCLImate.Ruby**](https://github.com/synesissoftware/libCLImate.Ruby) library;
 * the [**Pantheios.Ruby**](https://github.com/synesissoftware/Pantheios.Ruby) library;
 * the [**recls.Ruby**](https://github.com/synesissoftware/recls.Ruby) library;
 * the [**xqsr3**](https://github.com/synesissoftware.com/xqsr3/);
 * the [**xqsr3-xml**](https://github.com/synesissoftware.com/xqsr3-xml/) library;
+
+
+<!-- ########################### end of file ########################### -->
 
--- bash/run_all_unit_tests.sh/python/run_all_unit_tests.sh
+++ bash/run_all_unit_tests.sh/python/run_all_unit_tests.sh
@@ -7,25 +7,57 @@
 #           calling directory
 #
 # Created:  13th February 2019
-# Updated:  10th December 2023
+# Updated:  2nd April 2024
 #
-# Author:   Matthew Wilson
+# Copyright (c) Matthew Wilson, 2019-2024
+# All rights reserved
+#
+# Redistribution and use in Source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the names of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # ######################################################################## #
 
 
-source="${BASH_SOURCE[0]}"
-while [ -h "$source" ]; do
-	dir="$(cd -P "$(dirname "$source")" && pwd)"
-	source="$(readlink "$source")"
-	[[ $source != /* ]] && source="$dir/$source"
+# constants
+
+Source="${BASH_SOURCE[0]}"
+while [ -h "$Source" ]; do
+
+  Dir="$(cd -P "$(dirname "$Source")" && pwd)"
+  Source="$(readlink "$Source")"
+  [[ $Source != /* ]] && Source="$Dir/$Source"
 done
-dir="$( cd -P "$( dirname "$source" )" && pwd )"
+Dir="$(cd -P "$( dirname "$Source" )" && pwd)"
 
 
-# This will operate recursively as long as each subdirectory of $dir/tests
+# This will operate recursively as long as each subdirectory of $Dir/tests
 # contains an __init__.py file (which may be empty)
-python -m unittest discover "$dir/tests"
+python -m unittest discover "$Dir/tests"
+
 
 # ############################## end of file ############################# #
 
